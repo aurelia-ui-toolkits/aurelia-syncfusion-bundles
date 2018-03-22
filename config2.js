82e648953aa5774c91d9cead703212d66cf947c4
karthickthangasamy <karthickt@syncfusion.com>
2018-03-22 12:03:56 +0530

    package added

--- config2.js
+++ config2.js
@@ -10,10 +10,10 @@ System.config({
     "optional": []
   },
   paths: {
-    "github:*": "https://cdn.rawgit.com/aurelia-ui-toolkits/aurelia-syncfusion-bundles/0.0.4/jspm_packages/github/*",
-    "npm:*": "https://cdn.rawgit.com/aurelia-ui-toolkits/aurelia-syncfusion-bundles/0.0.4/jspm_packages/npm/*", 
-    "bundles/bundle.js": "https://cdn.rawgit.com/aurelia-ui-toolkits/aurelia-syncfusion-bundles/0.0.4/bundles/bundle.js"
-    "syncfusion-javascript/*": "https://rawgit.com/aurelia-ui-toolkits/aurelia-syncfusion-bundles/0.0.4/npm/syncfusion-javascript@16.1.26/*",
+    "github:*": "https://cdn.rawgit.com/aurelia-ui-toolkits/aurelia-syncfusion-bundles/0.0.5/jspm_packages/github/*",
+    "npm:*": "https://cdn.rawgit.com/aurelia-ui-toolkits/aurelia-syncfusion-bundles/0.0.5/jspm_packages/npm/*", 
+    "bundles/bundle.js": "https://cdn.rawgit.com/aurelia-ui-toolkits/aurelia-syncfusion-bundles/0.0.5/bundles/bundle.js"
+    "syncfusion-javascript/*": "https://rawgit.com/aurelia-ui-toolkits/aurelia-syncfusion-bundles/0.0.5/npm/syncfusion-javascript@16.1.26/*",
   },
   map: {
     "aurelia-animator-css": "npm:aurelia-animator-css@1.0.4",
